e9a5b02302e918aca65add72737988912e38157f
Graham Benevelli <gbenevelli@schoology.com>
2017-12-18 10:08:14 -0600

    Added tests

--- src/test/java/com/interview/AggregationQuestionTest.java
+++ src/test/java/com/interview/AggregationQuestionTest.java
@@ -87,4 +87,81 @@ public class AggregationQuestionTest {
 		assertTrue(actual.get(sally).containsKey("likes"));
 		assertEquals(expected, actual);
 	}
+
+	@Test
+	public void testAggregate2() throws Exception {
+		List<User> users = Lists.<User>newArrayList(
+				new BasicUser(1, "bob"),
+				new BasicUser(2, "sally")
+		);
+
+		List<Metric> metric = Lists.<Metric>newArrayList(
+				new BasicMetric(1, "likes", 10),
+				new BasicMetric(1, "views", 98),
+				new BasicMetric(1, "clicks", 390),
+				new BasicMetric(2, "likes", 56),
+				new BasicMetric(2, "views", 76),
+				new BasicMetric(2, "clicks", 23)
+		);
+
+		Map<User, Map<String, Long>> actual = this.aggregationQuestion.aggregate(metric, users);
+
+		Map<User, Map<String, Long>> expected = ImmutableMap.<User, Map<String, Long>>builder()
+				.put(new BasicUser(1, "bob"), ImmutableMap.<String, Long>builder()
+						.put("likes", 10l)
+						.put("views", 98l)
+						.put("clicks", 390l)
+						.build())
+				.put(new BasicUser(2, "sally"), ImmutableMap.<String, Long>builder()
+						.put("likes", 56l)
+						.put("views", 76l)
+						.put("clicks", 23l)
+						.build())
+				.build();
+
+		assertNotNull(actual);
+		assertTrue(actual.containsKey(new BasicUser(1, "bob")));
+		assertTrue(actual.containsKey(new BasicUser(2, "sally")));
+		assertTrue(actual.get(new BasicUser(1, "bob")).containsKey("likes"));
+		assertTrue(actual.get(new BasicUser(2, "sally")).containsKey("likes"));
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testAggregateDuplicate() throws Exception {
+		List<User> users = Lists.<User>newArrayList(
+				new BasicUser(1, "bob"),
+				new BasicUser(2, "sally")
+		);
+
+		List<Metric> metric = Lists.<Metric>newArrayList(
+				new BasicMetric(1, "likes", 10),
+				new BasicMetric(1, "likes", 98),
+				new BasicMetric(1, "clicks", 390),
+				new BasicMetric(2, "likes", 56),
+				new BasicMetric(2, "views", 76),
+				new BasicMetric(2, "clicks", 23)
+		);
+
+		Map<User, Map<String, Long>> actual = this.aggregationQuestion.aggregate(metric, users);
+
+		Map<User, Map<String, Long>> expected = ImmutableMap.<User, Map<String, Long>>builder()
+				.put(new BasicUser(1, "bob"), ImmutableMap.<String, Long>builder()
+						.put("likes", 108l)
+						.put("clicks", 390l)
+						.build())
+				.put(new BasicUser(2, "sally"), ImmutableMap.<String, Long>builder()
+						.put("likes", 56l)
+						.put("views", 76l)
+						.put("clicks", 23l)
+						.build())
+				.build();
+
+		assertNotNull(actual);
+		assertTrue(actual.containsKey(new BasicUser(1, "bob")));
+		assertTrue(actual.containsKey(new BasicUser(2, "sally")));
+		assertTrue(actual.get(new BasicUser(1, "bob")).containsKey("likes"));
+		assertTrue(actual.get(new BasicUser(2, "sally")).containsKey("likes"));
+		assertEquals(expected, actual);
+	}
 }
